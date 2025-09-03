06ad90b36ebc6a34a18c425a647f358db9f3f435
Natan De Aragão Müller <natan.muller@edu.pucrs.br>
2025-09-03 09:57:54 -0300

    exercicios finalizados

--- Exercicio001/Exercicio001.c
+++ Exercicio001/Exercicio001.c
@@ -1,5 +1,5 @@
 /*
-Exercicio001.c, feito por Natan de Aragão Müller no dia 03/09/2025 às xxhxx.
+Exercicio001.c, feito por Natan de Aragão Müller no dia 03/09/2025 às 09h47.
 
 Enunciado: Escreva um programa em C que, inicialmente, leia o nome de um arquivo (com no m  ́aximo 17 caracteres  ́uteis).
 A seguir, seu programa dever  ́a abrir o arquivo para leitura em modo texto, lendo um n  ́umero inteiro, que cor-
@@ -9,3 +9,38 @@ deve imprimir a mensagem “ARQUIVO INEXISTENTE” (encerrando o programa com c 
 0).
 
 */
+
+#include <stdio.h>
+
+int main () {
+
+    FILE *file;
+    char nome[18];
+    int qtdInteiros = 0;
+    int soma = 0;
+    int valor = 0;
+    scanf("%17s", nome);
+
+    file = fopen(nome, "r");
+    if(file == NULL) { 
+        printf("ARQUIVO INEXISTENTE");
+        return 0;
+    }
+
+    if (fscanf(file, "%d", &qtdInteiros) != 1) {
+        fclose(file); 
+        return 0; 
+    }
+
+    for (int i = 0; i < qtdInteiros; i++) { 
+        if (fscanf(file, "%d", &valor) == 1) { 
+            soma += valor; 
+        } 
+    }
+
+    fclose(file);
+
+    printf("%d", soma);
+
+    return 0;
+}
--- Exercicio002/Exercicio002.c
+++ Exercicio002/Exercicio002.c
@@ -1,5 +1,5 @@
 /*
-Exercicio002.c, feito por Natan de Aragão Müller no dia 03/09/2025 às xxhxx.
+Exercicio002.c, feito por Natan de Aragão Müller no dia 03/09/2025 às 09h57.
 
 Enunciado: Escreva um programa em C que, inicialmente, leia o nome de um arquivo (com no m  ́aximo 17 caracteres  ́uteis).
 A seguir, seu programa dever  ́a abrir o arquivo para leitura em modo bin  ́ario, lendo um n  ́umero inteiro, que
@@ -9,3 +9,38 @@ deve imprimir a mensagem “ARQUIVO INEXISTENTE” (encerrando o programa com c 
 0). Todos os valores lidos devem ser valores inteiros de 4 bytes, lidos no modo bin  ́ario
 
 */
+
+#include <stdio.h>
+
+int main () {
+
+    FILE *file;
+    char nome[18];
+    int qtdInteiros = 0;
+    int soma = 0;
+    int valor = 0;
+    scanf("%17s", nome);
+
+    file = fopen(nome, "r");
+    if(file == NULL) { 
+        printf("ARQUIVO INEXISTENTE");
+        return 0;
+    }
+
+    if (fread(&qtdInteiros, sizeof(int), 1, file) != 1) { 
+        fclose(file); 
+        return 0; 
+    }
+
+    for (int i = 0; i < qtdInteiros; i++) { 
+        if (fread(&valor, sizeof(int), 1, file) == 1) { 
+            soma += valor; 
+        } 
+    }
+
+    fclose(file);
+
+    printf("%d", soma);
+
+    return 0;
+}

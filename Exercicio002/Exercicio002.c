b13e3caad724d4dc4446907b05159118dcc21841
Natan De Aragão Müller <natan.muller@edu.pucrs.br>
2025-09-02 23:38:00 -0300

    adicionado enunciados apenas, amanha vou codar

---
+++ Exercicio002/Exercicio002.c
@@ -0,0 +1,11 @@
+/*
+Exercicio002.c, feito por Natan de Aragão Müller no dia 03/09/2025 às xxhxx.
+
+Enunciado: Escreva um programa em C que, inicialmente, leia o nome de um arquivo (com no m  ́aximo 17 caracteres  ́uteis).
+A seguir, seu programa dever  ́a abrir o arquivo para leitura em modo bin  ́ario, lendo um n  ́umero inteiro, que
+corresponde `a quantidade de valores inteiros (que aparecem na sequˆencia dentro desse mesmo arquivo), e cada
+um desses valores, calculando e mostrando o somat  ́orio desses valores. Caso o arquivo N  ̃AO exista, seu programa
+deve imprimir a mensagem “ARQUIVO INEXISTENTE” (encerrando o programa com c  ́odigo de sucesso, ou seja,
+0). Todos os valores lidos devem ser valores inteiros de 4 bytes, lidos no modo bin  ́ario
+
+*/
